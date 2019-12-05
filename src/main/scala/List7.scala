339884b8e3a6ef19701f0f4b231ed63becde2951
Jedrzej Rochala <jedrzej@Jedrzejs-MacBook-Pro.local>
2019-12-05 13:39:06 +0100

    add List 7 and rename previous Lists

---
+++ src/main/scala/List7.scala
@@ -0,0 +1,44 @@
+import scala.collection.mutable.LinkedHashSet
+
+object List7 extends App {
+  def repeatList[A](inputList: List[A], repeatCounter: List[Int]): List[A] = {
+    if (repeatCounter.exists(y => y < 0))
+      throw new IllegalArgumentException("U cant repeat 0 times") //tak sobie zalozylem / gdyby jednak Pan mial na mysli inaczej to wystarczy zmienic tutaj na <= i 3 case minimalnie edytowac
+    (inputList, repeatCounter) match {
+      case (Nil, _)  => Nil
+      case (in, Nil) => in
+      case (h :: t, hr :: tr) =>
+        h :: (if (hr == 1) repeatList(t, tr)
+              else repeatList(inputList, hr - 1 :: tr))
+    }
+  }
+
+  def repeatListWithoutDuplicates[A](
+      inputSet: LinkedHashSet[A],
+      repeatCounter: List[Int]
+  ): List[A] = {
+    if (repeatCounter.exists(y => y < 0))
+      throw new IllegalArgumentException("U cant repeat 0 times") //tak sobie zalozylem / gdyby jednak Pan mial na mysli inaczej to wystarczy zmienic tutaj na <= i 3 case minimalnie edytowac
+    if (inputSet.isEmpty) Nil
+    else if (repeatCounter.isEmpty) inputSet.toList
+    else
+      inputSet.head :: (if (repeatCounter.head == 1)
+                          repeatListWithoutDuplicates(
+                            inputSet.tail,
+                            repeatCounter.tail
+                          )
+                        else
+                          repeatListWithoutDuplicates(
+                            inputSet,
+                            repeatCounter.head - 1 :: repeatCounter.tail
+                          ))
+  }
+
+  println(repeatList(List(1, 2, 3, 4, 5), List(2, 4, 6)))
+  println(
+    repeatListWithoutDuplicates(
+      LinkedHashSet(2, 3, 4, 6, 8),
+      List(2, 2, 2, 2, 2)
+    )
+  )
+}
